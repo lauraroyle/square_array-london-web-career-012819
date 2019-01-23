2f169f72e09ebf556ea293f870717fc69b6e22e3
Laura Royle <lauraamie.royle@gmail.com>
2019-01-23 19:26:12 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(array)
   new_numbers = [] # empty array to store elements
-  array.each { |i| new_numbers << i ** 2 } # use index of array and shovel method
+  array.each { |i| new_numbers << i ** 2 } # use index of array within pipes placeholder and shovel method
   return new_numbers # call method
 end
