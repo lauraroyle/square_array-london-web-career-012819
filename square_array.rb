cb7e13bb4b7dbb35f90d78af9b304e6bde66d22e
Laura Royle <lauraamie.royle@gmail.com>
2019-01-23 19:04:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,17 @@
-def square_array(numbers)
-  numbers.each do {numbers**}
-   puts numbers
-   numbers += 1
-end
+#def square_array(numbers)
+#  numbers.each do | []** |
+#   puts numbers
+#   numbers += 1
+# end
+
+def square_array(array)
+    square_array = []
+    array.each do |num|
+      square = num ** 2
+      squared_array << square
+    end
+
+    squared_array
+
+  end
+      
